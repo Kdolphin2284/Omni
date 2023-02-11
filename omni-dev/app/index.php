aa391177d791a63b74cdae7e4f72a94d97c6e775
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2023-02-11 16:49:57 -0500

    Large Commit - Omni Development
    - CSS almost 100% matches Omni branding
    - Node animations
    - Modal implementation

--- omni-dev/app/index.php
+++ omni-dev/app/index.php
@@ -17,17 +17,255 @@ $milliseconds = intval(microtime(true) * 1000);
     <!-- <header class="navigation"> -->
       <!-- <a href="/">Omni</a> -->
     <!-- </header> -->
-    <a href="/" id="home-element-1">
+    <!-- <a href="/" id="home-element-1">
         <h1 id="OmniLogo">Omni</h1>
-    </a>
-    <form id="home-element-2">
+    </a> -->
+
+    <!-- THIS IS WHERE IT STARTS -->
+    <a href="https://omnimusic.co/omni-dev/app/" id="homepageLink"></a>
+    <div class="container-artist-popup flex-column">
+        <div class="flex-between">
+            <button onclick="modalGone()" class="btn">
+                <img src="images/material-symbols_close.svg" class="icon">
+            </button>
+            <button onclick="myFunction()" class="btn">
+                <span>Open on Spotify</span>
+                <img src="images/spotify.svg" class="icon">
+            </button>
+        </div>
+        <div class="artist-basic-info flex-column">
+            <img src="#" alt="" class="artist-image">
+            <h4 class="artist-name">Artist Name</h4>
+            <div class="flex-horizontal-16">
+                <button onclick="myFunction()" class="btn">
+                    <img src="images/overview.svg" class="icon">
+                    <span>Overview</span>
+                </button>
+                <button onclick="myFunction()" class="btn">
+                    <img src="images/user.svg" class="icon">
+                    <span>About</span>
+                </button>
+            </div>
+        </div>
+        <div class="artist-popular-songs container-secondary flex-column">
+            <h2 class="h2-title">Popular Songs</h2>
+            <div class="container-artists-songs flex-column">
+                <div class="artist-song flex-between">
+                    <div>
+                        <img src="#" alt="" class="album-cover-sm">
+                        <div class="song-name flex-column">
+                            <p>Song name</p>
+                            <img src="#" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <p>1,000,000,000</p>
+                        <p>3:33</p>
+                    </div>
+                </div>
+                <div class="artist-song flex-between">
+                    <div>
+                        <img src="#" alt="" class="album-cover-sm">
+                        <div class="song-name">
+                            <p>Song name</p>
+                            <img src="#" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <p>1,000,000,000</p>
+                        <p>3:33</p>
+                    </div>
+                </div>
+                <div class="artist-song flex-between">
+                    <div>
+                        <img src="#" alt="" class="album-cover-sm">
+                        <div class="song-name">
+                            <p>Song name</p>
+                            <img src="#" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <p>1,000,000,000</p>
+                        <p>3:33</p>
+                    </div>
+                </div>
+                <div class="artist-song flex-between">
+                    <div>
+                        <img src="#" alt="" class="album-cover-sm">
+                        <div class="song-name">
+                            <p>Song name</p>
+                            <img src="#" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <p>1,000,000,000</p>
+                        <p>3:33</p>
+                    </div>
+                </div>
+                <div class="artist-song flex-between">
+                    <div>
+                        <img src="#" alt="" class="album-cover-sm">
+                        <div class="song-name">
+                            <p>Song name</p>
+                            <img src="#" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <p>1,000,000,000</p>
+                        <p>3:33</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-secondary flex-column">
+            <h2 class="h2-title">Discography</h2>
+            <div class="flex-horizontal-16">
+                <button onclick="myFunction()" class="btn">
+                    <img src="images/album.svg" class="icon">
+                    <span>Albums</span>
+                </button>
+                <button onclick="myFunction()" class="btn">
+                    <img src="images/music.svg" class="icon">
+                    <span>Singles & EPs</span>
+                </button>
+                <button onclick="myFunction()" class="btn">
+                    <img src="images/playlist.svg" class="icon">
+                    <span>Playlists</span>
+                </button>
+            </div>
+            <div id="album-container" class="container-albums">
+                <div class="album flex-column">
+                    <img src="#" alt="" class="album-cover-lg">
+                    <p class="album-name">Album Name</p>
+                    <p class="album-year">2017</p>
+                </div>
+                <div class="album flex-column">
+                    <img src="#" alt="" class="album-cover-lg">
+                    <p class="album-name">Album Name</p>
+                    <p class="album-year">2017</p>
+                </div>
+                <div class="album flex-column">
+                    <img src="#" alt="" class="album-cover-lg">
+                    <p class="album-name">Album Name</p>
+                    <p class="album-year">2017</p>
+                </div>
+                <div class="album flex-column">
+                    <img src="#" alt="" class="album-cover-lg">
+                    <p class="album-name">Album Name</p>
+                    <p class="album-year">2017</p>
+                </div>
+                <div class="album flex-column">
+                    <img src="#" alt="" class="album-cover-lg">
+                    <p class="album-name">Album Name</p>
+                    <p class="album-year">2017</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="nav-container">
+        <!-- <img src="images/logo-lg.svg" alt="logo"> -->
+            <div class="nav-content">
+                <div class="user-button">
+                    
+                    <div class="dropdown">
+                        <button onclick="myFunction()" class="btn dropbtn">
+                            <img class="icon" src="images/spotify.svg">
+                            <span>aidanty23</span>
+                            <img class="icon" src="images/down-med.svg">
+                        </button>
+                        <!-- <div id="myDropdown" class="dropdown-content">
+                            <a href="#">Link 1</a>
+                        </div> -->
+                    </div>
+                </div>
+                <button onclick="myFunction()" class="btn dropbtn">
+                    <img class="icon" src="images/help.svg" alt="">
+                </button>
+            </div>
+        </div>
+        <div class="content-main flex-column" id="search-triggers-none">
+            <div class="container-secondary flex-column">
+                <div class="logo" id="logo">
+                    <!-- <h1 class="h1-title">Omni</h1> -->
+                    <img src="images/logo-lg.svg" alt="logo" id="logo-img">
+                </div>
+                <form id="home-element-2">
+                    <img src="images/dark-search-md.svg" alt="search">
+                    <input type="text" id="query" placeholder="Search for an artist to discover similar artists">
+                    <button id="submit">Draw Graph</button>
+                </form>
+            </div>
+            <div class="container-secondary flex-column" id="recently-played">
+                <h2 class="h2-title">Recently played</h2>
+                <div class="artist-grid">
+                    <button class="btn">
+                        <img src="#" alt="" class="img-placeholder-sm">
+                        <p class="artistButtonText">The Weeknd</p>
+                    </button>
+                </div>
+            </div>
+            <div class="container-secondary flex-column" id="top-artists">
+                <div>
+                    <h2 class="h2-title">Top artists</h2>   
+                    <div class="dropdown">
+                        <button onclick="myFunction()" class="btn dropbtn">
+                            <span>Last 30 Days</span>
+                            <img class="icon" src="images/down-med.svg">
+                        </button>
+                        <!-- <div id="myDropdown" class="dropdown-content">
+                            <a href="#">Link 1</a>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="container-top-artists">
+                    <button class="btn-artist flex-column">
+                        <img src="#" alt="" class="img-placeholder-lg">
+                        <p class="artistButtonText">Nas</p>
+                    </button>
+                    <button class="btn-artist flex-column">
+                        <img src="#" alt="" class="img-placeholder-lg">
+                        <p class="artistButtonText">Nas</p>
+                    </button>
+                    <button class="btn-artist flex-column">
+                        <img src="#" alt="" class="img-placeholder-lg">
+                        <p class="artistButtonText">Nas</p>
+                    </button>
+                    <button class="btn-artist flex-column">
+                        <img src="#" alt="" class="img-placeholder-lg">
+                        <p class="artistButtonText">Nas</p>
+                    </button>
+                    <button class="btn-artist flex-column">
+                        <img src="#" alt="" class="img-placeholder-lg">
+                        <p class="artistButtonText">Nas</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- THIS IS WHERE IT ENDS -->
+    <!-- <form id="home-element-2">
         <input type="text" id="query" placeholder="Search for an artist to discover similar artists">
         <button id="submit">Draw Graph</button>
-    </form>
+    </form> -->
 
-    <a id="uri"></a>
+    <!-- <a id="uri"></a> -->
     <h2 id="message"></h2>
     <svg id="chart"></svg>
+    <div id="modal">
+        <div class='button' onclick="modalRight()"><img src="images/artistInfoIcon.png" alt="Artist Info Icon" /><span id="selected_artist_modal_name">Name</span></div>
+        <div id="extra-kids">
+        <div class="modal-flex">
+            <div class='button'><img src="images/artistInfluencesIcon.png" alt="Artist Info Icon">Influences</div>
+            <img src="images/artistArrowLeft.png" alt="Artist Info Icon">
+            <div class='button'><img src="images/similarArtistIcon.png" alt="Artist Info Icon">Similar Artists</div>
+            <img src="images/artistArrowRight.png" alt="Artist Info Icon">
+            <div class='button'><img src="images/influencedArtistIcon.png" alt="Artist Info Icon">Influenced</div>
+        </div>
+</div>
+    </div> 
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"></script>
--- omni-dev/app/main.js
+++ omni-dev/app/main.js
@@ -8,6 +8,8 @@ function stringGen(len) {
   }
   
 let MY_AUTH_TOKEN = "";
+let SELECTED_ARTIST = "";
+let SELECTED_NODE;
 
 let MY_ID = "";
 window.onload = function (w) {
@@ -46,13 +48,16 @@ function addToGraph(graphObj, artist, nodeObj, linkObj) {
     return graphObj;
 }
 
+let EXPANDED_NODE_SIZE = 102.5;
+
 function buildGraph(originalArtist, artists, existingGraph) {
     var graphObj = existingGraph || {
-        nodes: [{id: originalArtist.name, uri: originalArtist.uri, image: originalArtist.images[0].url, uuid: originalArtist.id}],
+        nodes: [{id: originalArtist.name, uri: originalArtist.uri, image: originalArtist.images[0].url, uuid: originalArtist.id, original: true, rad: EXPANDED_NODE_SIZE}],
         links: []
     };
     graphObj = artists.reduce(function(accum, artist) {
-        var nodeObj = {id: artist.name, uri: artist.uri, image: artist.images[0].url, uuid: artist.id};
+        // var nodeObj = {id: artist.name, uri: artist.uri, image: artist.images[0].url, uuid: artist.id, original: false, rad: EXPANDED_NODE_SIZE / 2};
+        var nodeObj = {id: artist.name, uri: artist.uri, image: artist.images[0].url, uuid: artist.id, original: false, rad: 62.5};
         var linkObj = {source: originalArtist.name};
         return addToGraph(graphObj, artist, nodeObj, linkObj);
     }, graphObj);
@@ -82,7 +87,7 @@ function getFirstDegreeArtists(response) {
         $("#message").text("¯\\_(ツ)_/¯ Can't find that artist - try again.");
     }
     var artist = response.artists.items[0];
-    $("#uri").attr("href", artist.uri).text($("#query").val());
+    $("#uri").attr("href", artist.uri).text(artist.name);
     return $.when(artist, getRelatedArtists(artist.id)
         .then(function(response) { 
             if(response.artists.length > maxArists){
@@ -128,17 +133,108 @@ function buildFirstGraph(originalArtist, firstDegreeArtists) {
 
 //     return updatedGraph;
 // }
+
+function moveGraphToCenter(x, y){
+    simulation.force("center", d3.forceCenter(x, y));
+    currentGraphPosition = [x, y];
+}
+
+function setupGraph(x, y){
+    targetGraphCenter = [x, y];
+    currentGraphPosition = [x, y];
+    startGraphPosition = [x, y];
+    moveGraphToCenter(x, y);
+}
+
+function animateGraphToCenter(x, y) {
+    startGraphPosition = [currentGraphPosition[0], currentGraphPosition[1]];
+    targetGraphCenter = [x, y];
+    graphMoveTick = 0;
+}
+
+var currentGraphPosition = [0,0];
+var targetGraphCenter = [0,0];
+var startGraphPosition = [0,0];
+function showModal(artist, x, y) {
+    if(artist.original){
+        animateGraphToCenter(defaultMiddleX, defaultMiddleY);
+    }
+    else{
+        animateGraphToCenter(chartWidth * 3 / 4, chartHeight / 2);
+    }
+    // modalRight();
+    $("#selected_artist_modal_name").html(artist.id);
+    SELECTED_NODE = artist;
+    // $("#modal").html(JSON.stringify(artist));
+    $("#modal").css("top", y);
+    $("#modal").css("left", x);
+    $("#modal").css("display", "block");
+}
+
+function updateModal(artist, x, y) {
+    if(artist == null){
+        $("#modal").css("display", "none");
+        return;
+    }
+    $("#modal").css("display", "block");
+    if(artist.original){
+        $("#extra-kids").css("display", "none");
+    }
+    else{
+        $("#extra-kids").css("display", "block");
+        //x = is the middle
+        //x - (EXPANDED_NODE_SIZE / 2) - (1.5)
+    }
+    let leftX = ((x - 1.5)) - ($("#modal").width() / 2);
+    console.log($("#modal").width());
+    $("#modal").css("top", y + (EXPANDED_NODE_SIZE * 1.25));
+    $("#modal").css("left", leftX);
+}
+function easeInOutQuad(x) {
+    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+}
+
+function smoothLerp(startX, startY, targetX, targetY, percent, animationCurve){
+    let x = (startX * (1.0 - animationCurve(percent))) + (targetX * (animationCurve(percent)));
+    let y = (startY * (1.0 - animationCurve(percent))) + (targetY * (animationCurve(percent)));
+    return [x, y];
+}
+
+let animationTick = 0;
+let startPosition = [];
+let maxAnimationTime = 20.0;
+let graphMoveTick = 0;
+
+function getSizeOfNode(node) {
+    if(node.original || node.id == SELECTED_ARTIST){
+        return EXPANDED_NODE_SIZE;
+    }
+    return node.rad;
+}
+
+
+var simulation;
+var chartWidth;
+var chartHeight;
+
+var defaultMiddleX;
+var defaultMiddleY;
+
 function drawGraph(graph){
     graph.nodes = _.uniqBy(graph.nodes, "id");
     var svg = d3.select("svg"),
         width = parseInt(d3.select("#chart").style("width")),
-        height = parseInt(d3.select("#chart").style("height"))
-
-    var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        height = parseInt(d3.select("#chart").style("height"));
+    chartWidth = width;
+    chartHeight = height;
+    defaultMiddleX = width / 2;
+    defaultMiddleY = height / 2 - 20;
+    simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(function(d) {return 0.5;}))
         // .force("charge", d3.forceManyBody().strength(-100))
         .force("charge", d3.forceManyBody().strength(-20000))
-        .force("center", d3.forceCenter(width / 2, height / 2.5));
+        .force("center", d3.forceCenter(defaultMiddleX, defaultMiddleY));
+    setupGraph(defaultMiddleX, defaultMiddleY);
 
     var div = d3.select("body").append("div")
         .attr("class", "tooltip")
@@ -154,7 +250,7 @@ function drawGraph(graph){
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
-            div.html(d.source.id + " --- " + d.target.id)
+            div.html(d.source.id + " ---> " + d.target.id)
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
         })
@@ -180,19 +276,19 @@ function drawGraph(graph){
         .attr("preserveAspectRatio", "none");
     }
 
-    console.log(graph.nodes);
 
-
-    var node = svg.append("g")
+        var node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
         .data(graph.nodes)
         .enter()
         .append("circle")
-        .attr("r", 128)
+        .attr("r", function (d) {return getSizeOfNode(d);})
         .style("fill", function(d) {return "url(#" + d.uuid + ")"})
         .on("mouseover", function(d) {
-            d3.select(this).transition().duration(200).attr("r", 160)
+            d.hovered = true;
+            d3.select(this).transition().duration(200)
+            .attr("r", function (d) {return EXPANDED_NODE_SIZE;})
             div.transition()
                 .duration(200)
                 .style("opacity", .9)
@@ -201,22 +297,37 @@ function drawGraph(graph){
                 .style("top", (d3.event.pageY - 28) + "px");
         })
         .on("mouseout", function(d) {
-            d3.select(this).transition().duration(200).attr("r", 128)
+            d3.select(this).transition().duration(200)
+            .attr("r", function (d) {
+                if(SELECTED_ARTIST == d.id){
+                    return EXPANDED_NODE_SIZE;
+                }
+                return d.rad})
+            .on("end", function(der){
+                d.hovered = false;
+            });
             div.transition()
                 .duration(500)
                 .style("opacity", 0);
         })
         .on("click", function(d) {
-            $("#query").val(d.id);
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
-            visuify()
+        
+            showModal(d, d.x, d.y);
+            SELECTED_ARTIST = d.id;
+            animationTick = 0;
+            startPosition = [d.x, d.y];
+            // visuify();
+            // $("#query").val(d.id);
+            // div.transition()
+            //     .duration(500)
+            //     .style("opacity", 0);
+            // visuify()
         })
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
+    
 
             // node.append("image")
             // .attr("xlink:href", 'http://placekitten.com/g/48/48')
@@ -232,17 +343,96 @@ function drawGraph(graph){
     simulation.force("link")
         .links(graph.links);
 
+    
+
+
+    
     function ticked() {
         console.log("UPDATED 1");
+        animationTick++;
+        graphMoveTick++;
+        if(graphMoveTick > maxAnimationTime){
+            graphMoveTick = maxAnimationTime;
+        }
+        if(animationTick > maxAnimationTime){
+            animationTick = maxAnimationTime;
+        }
+        if(currentGraphPosition[0] != targetGraphCenter[0] || currentGraphPosition[1] != targetGraphCenter[1]){
+            let percent = graphMoveTick / maxAnimationTime;
+            let result = smoothLerp(startGraphPosition[0], startGraphPosition[1], targetGraphCenter[0], targetGraphCenter[1], percent, easeInOutQuad);
+            moveGraphToCenter(result[0], result[1]);
+        }
+        
+        let nX = -1;
+        let nY = -1;
+        let SELECTED_NODE_ANCHOR_X = 350;
+        let SELECTED_NODE_ANCHOR_Y = chartHeight / 2 - (EXPANDED_NODE_SIZE);
+        let artistJawn = null;
+        let animationPosition;
+        if(SELECTED_ARTIST != null){
+            animationPosition = smoothLerp(startPosition[0], startPosition[1], SELECTED_NODE_ANCHOR_X, SELECTED_NODE_ANCHOR_Y, animationTick / maxAnimationTime, easeInOutQuad);
+        }
+        node
+            .attr("cx", function(d) { 
+                if(d.id == SELECTED_ARTIST && !d.original){
+                    artistJawn = d;
+                    nX = animationPosition[0];
+                    return animationPosition[0];
+                }
+                else if(d.id == SELECTED_ARTIST){
+                    nX = d.x;
+                }
+                if(d.id == SELECTED_ARTIST){
+                    artistJawn = d;
+                }
+                return d.x; 
+            })
+            .attr("cy", function(d) { 
+                if(d.id == SELECTED_ARTIST && !d.original) {
+                    nY = animationPosition[1];
+                    return animationPosition[1];
+                }
+                else if(d.id == SELECTED_ARTIST){
+                    nY = d.y;
+                }
+                return d.y; 
+            })
+            .attr("r", function (d) {
+                if(d.hovered == true){
+                    return d3.select(this).attr("r");
+                }
+                return getSizeOfNode(d);
+            })
+            .style("stroke", function(d) {
+                if(d.id == SELECTED_ARTIST){
+                    return "#FFFFFF";
+                }
+                else{
+                    return "transparent";
+                }
+            })
+            ;
+            
         link
             .attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+            .attr("x2", function(d) { 
+                if(d.target.id == SELECTED_ARTIST){
+                    return animationPosition[0];
+                }
+                return d.target.x; 
+            })
+            .attr("y2", function(d) { 
+                if(d.target.id == SELECTED_ARTIST){
+                    return animationPosition[1];
+                }
+                return d.target.y;
+            });
 
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        // let currentArtist = null;
+       
+            updateModal(artistJawn, nX, nY);
+        
 
             
             // pattern.attr("x", (function(d) { return d.x;}))
@@ -283,7 +473,6 @@ function drawGraph(graph){
 const OmniLogo = document.getElementById('OmniLogo');
 const uriPadding = document.getElementById('uri');
 // const homeElement1 = document.getElementById('home-element-1');
-// const homeElement2 = document.getElementById('home-element-2');
 
 function OmniLogoPlacement() {
     OmniLogo.style.position = "absolute";
@@ -302,6 +491,35 @@ function OmniLogoPlacement() {
     // homeElement2.style.top = "0";
 }
 
+const homeContent = document.getElementById('search-triggers-none');
+const homeElement2 = document.getElementById('home-element-2');
+const homeElement2Input = document.getElementById('query');
+const recentlyPlayed = document.getElementById('recently-played');
+const topArtists = document.getElementById('top-artists');
+const homepageLink = document.getElementById('homepageLink');
+const chart = document.getElementById('chart');
+
+const logo = document.getElementById('logo');
+const logoImg = document.getElementById('logo-img');
+
+function searchTriggersNone() {
+    chart.style.position = 'fixed';
+    homepageLink.style.display = 'inline-block';
+    homeElement2.style.position = 'absolute';
+    homeElement2.style.top = '48px';
+    homeElement2.style.left = '200px';
+    query.style.margin = '0';
+    query.style.height = '32px';
+    query.style.borderRadius = '100px';
+    recentlyPlayed.style.display = 'none';
+    topArtists.style.display = 'none';
+    logo.style.position = 'absolute';
+    logo.style.top = '48px';
+    logo.style.left = '48px';
+    logo.style.height = '44px';
+    logoImg.style.height = 'inherit';
+}
+
 // End Added Code
 
 function visuify(){
@@ -316,11 +534,36 @@ function visuify(){
         .then(drawGraph);
 }
 
+// function visuifyFromNode(){
+//     $("svg").empty();
+//     $("#message").empty();
+//     $("#uri").empty();
+//     getArtist($("#selected_artist_modal_name").val())
+//         .then(getFirstDegreeArtists)
+//         .then(buildFirstGraph)
+//         .then(drawGraph);
+// }
+
 $("#submit").on("click", function(e) {
     e.preventDefault();
     visuify();
-    OmniLogoPlacement();
+    // OmniLogoPlacement();
+    searchTriggersNone();
 });
 
+function modalRight() {
+    $(".container-artist-popup").addClass("container-artist-selected");
+}
 
+function modalGone() {
+    $(".container-artist-popup").removeClass("container-artist-selected");
+}
+// const $bg = document.querySelector(".background");
+// const mouseScale = 0.25;
+// $bg.addEventListener("mousemove", e => {
+//     const x = e.offsetX / $bg.clientWidth * 100 - 50;
+//     const y = e.offsetY / $bg.clientHeight * 100 - 50;
+//     $bg.style.setProperty("--mouseX", `${(x * mouseScale).toFixed(3)}%`);
+//     $bg.style.setProperty("--mouseY", `${(y * mouseScale).toFixed(3)}%`);
+// });
 // 263, 264, 110, 89 (second degree artist information commented out starting on these 4 lines)
